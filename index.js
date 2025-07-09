42066778dcc6b7b7110227cb1e3045413e910e46
Amey Desai <amey26desai@gmail.com>
2025-07-09 16:55:24 +0530

    Allowed netlfy

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://el-shopping.netlify.app"], // Add Netlify domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // route configuration
 app.use("/api/products", productRoutes);
